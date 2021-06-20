952a40bc8d29b05abcf9ba9fddc5c7f5fe23d94f
ncucu <ncucu.me@kakaocommerce.com>
2021-06-20 15:39:19 +0900

    SCG Filter - Java Config

--- FirstService/src/main/java/me/june/firstservice/FirstServiceController.java
+++ FirstService/src/main/java/me/june/firstservice/FirstServiceController.java
@@ -1,15 +1,16 @@
 package me.june.firstservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/first-service")
 public class FirstServiceController {
 
 	@GetMapping("welcome")
-	public String welcome() {
+	public String welcome(@RequestHeader("first-request") String header) {
 		return "Welcome to the First Service";
 	}
 }
--- SecondService/src/main/java/com/example/secondservice/SecondServiceController.java
+++ SecondService/src/main/java/com/example/secondservice/SecondServiceController.java
@@ -1,15 +1,16 @@
 package com.example.secondservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/second-service")
 public class SecondServiceController {
 
 	@GetMapping("/welcome")
-	public String welcome() {
+	public String welcome(@RequestHeader("second-request") String header) {
 		return "Welcome to the Second Service";
 	}
 }
