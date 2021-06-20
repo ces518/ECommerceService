3619382c9ac25b55cc25862bc4afab555246ebb6
ncucu <ncucu.me@kakaocommerce.com>
2021-06-20 15:03:37 +0900

    Spring Cloud API Gateway Sample

---
+++ APIGatewayService/src/main/java/me/june/apigatewayservice/ApiGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package me.june.apigatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayServiceApplication.class, args);
+	}
+
+}
