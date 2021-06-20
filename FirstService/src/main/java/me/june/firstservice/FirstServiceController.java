92db0774d27ab90bd989c35a36617874bedbd7c3
ncucu <ncucu.me@kakaocommerce.com>
2021-06-20 20:42:24 +0900

    SCG - Eureka 연동

--- FirstService/src/main/java/me/june/firstservice/FirstServiceController.java
+++ FirstService/src/main/java/me/june/firstservice/FirstServiceController.java
@@ -1,10 +1,14 @@
 package me.june.firstservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
 @RestController
 @RequestMapping("/first-service")
 public class FirstServiceController {
@@ -15,7 +19,8 @@ public class FirstServiceController {
 	}
 
 	@GetMapping("check")
-	public String check() {
+	public String check(HttpServletRequest request) {
+		log.info("IP = {}", request.getRemoteAddr());
 		return "Hi, there. This is a message from First Service";
 	}
 }
