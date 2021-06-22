70bfdf488dc4028742426eb08abee7c76ec8133e
ncucu <ncucu.me@kakaocommerce.com>
2021-06-22 20:16:18 +0900

    ECommerce MSA - API Gateway UserService 등록

--- UserService/src/main/java/me/june/userservice/PingController.java
+++ UserService/src/main/java/me/june/userservice/PingController.java
@@ -1,13 +1,17 @@
 package me.june.userservice;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class PingController {
+	private final Environment env;
 
-	@GetMapping("/health_check")
+	@GetMapping("/user-service/health_check")
 	public String healthCheck() {
-		return "It's Working in User Service";
+		return String.format("It's Working in User Service", env.getProperty("local.server.port"));
 	}
 }
